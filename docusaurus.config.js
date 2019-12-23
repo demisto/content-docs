851a6f46707a83862e417b5c90e807d1fe32e345
Francesco Vigo <fvigo@paloaltonetworks.com>
2019-12-23 10:49:44 +0100

    Renamed Develop to "Development Guide" in menu. Created a structure with subtrees in sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
         },
         {
           to: "docs/dev-guide",
-          label: "Develop",
+          label: "Development Guide",
           position: "left"
         },
         {
--- sidebars.js
+++ sidebars.js
@@ -2,44 +2,74 @@ module.exports = {
   docs: [
     {
       type: "category",
-      label: "Develop",
+      label: "Development Guide",
       items: [
         {
           type: "category",
           label: "Design",
           items: [
             "use-cases",
-            'design-best-practices'
+            "design-best-practices"
           ]
         },
-        "dev-guide",
-        "why-demisto",
-        "dev-architecture",
-        "tutorial",
-        "dev-setup",
-        "docker",
-        "package-dir",
-        "context-and-outputs",
-        "dbot",
-        "dt",
-        "context-standards",
-        "playbooks",
-        "playbook-conventions",
-        "generic-polling",
-        "parameter-types",
-        "fetching-incidents",
-        "fetching-credentials",
-        "long-running",
-        "linting",
-        "testing",
-        "unit-testing",        
-        "circleci",
-        "debugging",
-        "code-conventions",
-        "changelog",
-        "integration-docs",
-        "doc-structure",
-        "yaml-file",
+        {
+          type: "category",
+          label: "Getting Started",
+          items: [
+            "dev-guide",
+            "why-demisto",
+            "dev-architecture",
+            "tutorial",
+            "package-dir"
+          ]
+        },
+        {
+          type: "category",
+          label: "Writing an integration",
+          items: [
+            "dev-setup",
+            "docker",
+            "code-conventions",   
+            "parameter-types",
+            "fetching-incidents",
+            "fetching-credentials",
+            "long-running", 
+            "context-and-outputs",
+            "dbot",
+            "dt",
+            "context-standards",
+            "yaml-file"                        
+          ]
+        },
+        {
+          type: "category",
+          label: "Writing Playbooks",
+          items: [
+            "playbooks",
+            "playbook-conventions",
+            "generic-polling"        
+          ]
+        },         
+        {
+          type: "category",
+          label: "Testing",
+          items: [
+            "linting",
+            "testing",
+            "unit-testing",        
+            "circleci",
+            "debugging"
+          ]
+        }, 
+        {
+          type: "category",
+          label: "Documentation",
+          items: [
+            "changelog",
+            "integration-docs",
+            "doc-structure"
+          ]
+        }
       ]
     }
   ]
