f78f395526216900fa035cc93febe1cd62d9004c
Francesco Vigo <fvigo@paloaltonetworks.com>
2020-03-05 16:36:46 +0100

    first working version of generate toc

--- generate_toc.ts
+++ generate_toc.ts
@@ -1,4 +1,3 @@
-
 import { readFileSync, createReadStream } from 'fs';
 
 const DOCSPREFIX: string='docs'
@@ -9,14 +8,49 @@ interface SideBarItem {
 	type: "category" | "doc",
 	label?: string,
 	id?: string,
-	tocpath?: string
 	items: Array<any>
 };
 
-interface Document {
-	id: string,
-	title: string,
+class Document {
+	id: string;
+	title: string;
+	constructor(id?: string, title?: string) {
+		this.id = id? id : 'N/A';
+		this.title = title? title : 'N/A';
+	}
 };
+
+class TOC {
+	id: string;
+	documents: Array<Document>;
+	tocs: Array<TOC>;
+	constructor(id?: string) {
+		this.id = id? id : 'N/A';
+		this.documents = new Array<Document>();
+		this.tocs = new Array<TOC>();
+	}
+}
+
+function TOCToMarkDown(toc: TOC, depth: number = 0): string {
+	let md: string = "";
+	let hashes: string = '#'.repeat(depth+1);
+	// title
+	md += hashes + ' ' + toc.id[0].toUpperCase() + toc.id.slice(1) + '\n\n';
+
+	// add  all documents first
+	for(let doc of toc.documents) {
+		md+= '[' + doc.title + ']' + '(' + doc.id + ')\n';
+	}
+
+	md+= '\n';
+
+	// ad all TOCs later
+	for(let t of toc.tocs) {
+		md += TOCToMarkDown(t, depth+1);
+	}
+	
+	return md;
+}
 
 function extractFileData(f): Document | undefined {
 	// console.log(s)
@@ -25,51 +59,56 @@ function extractFileData(f): Document | undefined {
 	// console.log('opening file ' + fileName)
 	let data = readFileSync(fileName,{ encoding: 'utf-8' }).split('\n').filter(Boolean)
 
-	let document: Document = {} as Document;
+	let document: Document = new Document();
+
 	if(data[0] === '---' && data[3] === '---') {
 		if(data[1].startsWith('id: ')) {
 			document.id = data[1].split(': ')[1].trim()
 		}
-		else document.id = "N/A"
 		if(data[2].startsWith('title: ')) {
 			document.title = data[2].split(': ')[1].trim()
 		}
-		else document.title = "N/A"
 	}
 	return document;
 }
 
-function build_toc(tocpath: string, label: string, items: Array<SideBarItem>) {
-	let documents: Array<Document> = []
+function build_toc(label: string, items: Array<SideBarItem>): TOC {
+	let toc : TOC = new TOC(label);
+	toc.id = label;
+
 	for(let i of items) {
-		// console.log('i is:' + JSON.stringify(i))
-		let doc:string = ""
-		if(typeof(i) === 'object' && 'type' in i && 'tocpath' in i &&  i.type === "category") {
-			build_toc(tocpath + '/' + i.tocpath, label + '/' + i.label, i.items);
-			continue;
+
+		// element is a category: add it as a new TOC
+		if(typeof(i) === 'object' && 'type' in i && i.type === "category") {
+			toc.tocs.push(build_toc(label + '/' + i.label, i.items));
 		}
+
+		// element is a document: add is as a new Document
 		else if(typeof(i) === 'object' && 'type' in i && i.type == "doc"){
-			doc = i.id;
+			toc.documents.push(extractFileData(i.id))
 		}
+
+		// element is a string: add it as a document
 		else if (typeof(i) === 'string') {
-			doc = i;
+			toc.documents.push(extractFileData(i))
 		}
+		// skip something unknown
 		else {
 			// console.log("Skipping object: " + JSON.stringify(i))
 			continue;
 		}
-		if(doc === tocpath) continue;
-		documents.push(extractFileData(doc))
 	}
-
-	console.log('Building TOC for label: ' + label + ' with tocpath: ' + DOCSPREFIX + '/' + tocpath + '.md\n' + JSON.stringify(documents) + '\n\n');
-
+	// console.log('Building TOC for label: ' + label + '\n' + JSON.stringify(toc.documents) + '\n\n');
+	return toc;
 }
 
 function parse_sidebars() {
 	// console.log(sidebars)
+	let toc: TOC = undefined;
 	for(let x in sidebars) {
-		build_toc(x, x, sidebars[x])
+		toc = build_toc(x, sidebars[x])
+		// console.log('TOC for sidebar ' + x + ' is: ' + JSON.stringify(toc))
+		console.log(TOCToMarkDown(toc))
 	}
 }
 
