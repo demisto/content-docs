eb7832f5a4a976a64054f622b899f2a820587647
yaron-libman <43783884+yaron-libman@users.noreply.github.com>
2020-04-02 20:29:59 +0300

    Updated sidebar and corrected typo
    We misspelled the name of the HashiCorp Vault product in the fetching credentials article.

--- sidebars.js
+++ sidebars.js
@@ -128,7 +128,8 @@ const sidebars = {
             "tutorials/tut-setup-dev",
             "tutorials/tut-integration-ui",
             "tutorials/tutorial-incident-lifecycle",
-            "tutorials/tutorial-playbook-TIM-EDL"
+            "tutorials/tutorial-playbook-TIM-EDL",
+            "tutorials/tutorial-TIM-export-indicators-to-3rd-party"
           ]
     }
   ],
