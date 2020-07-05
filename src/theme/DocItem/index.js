4216853058bbe60709d4a35e31ba851632fc2591
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-07-05 14:37:21 +0300

    create an issue where body contains links to site and source (#189)
    * create an issue where body contains links to site and source
    
    * use encodeURIComponent for title too
    
    * remove window.location for ssr support
    
    * fix lint

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -76,7 +76,35 @@ function DocItem(props) {
   } = DocContent;
 
   const issueTitle = `Issue with "${title}" in ${source}`;
-  const issueUrl = `https://github.com/demisto/content-docs/issues/new?labels=documentation&template=developer-documentation-issue.md&title=${issueTitle}`;
+  //IMPORTANT NOTE: if changing the body, note that the defacto max url length is 2000 chars for IE
+  const issueBody = `
+<!-- 
+Thank you for taking the time to help us improve our documentation! Please describe the problem and a suggested fix below and we'll get back to you as soon as we can.
+-->
+
+## Describe the problem
+
+* Page: [${title}](${siteConfig.url}${permalink})
+* Source: ${editUrl}
+
+<!--- Is this a typo, stale information, request for improvement, inaccuracy? -->
+<!--- Clearly and concisely describe the problem with the documentation -->
+
+
+## Screenshots
+<!-- If applicable, add screenshots to help explain your problem. -->
+
+## Environment
+ - OS: [e.g. Windows]
+ - Browser: [e.g. chrome, safari, firefox..]
+ - Browser Version:
+
+## Suggested fix
+
+<!--- If possible, help us by offering a suggested fix to the problem. If you know the fix, you may also submit a PR to fix the issue if you like! -->
+
+`;
+  const issueUrl = `https://github.com/demisto/content-docs/issues/new?labels=documentation&body=${encodeURIComponent(issueBody)}&title=${encodeURIComponent(issueTitle)}`;
   const metaImageUrl = siteUrl + useBaseUrl(metaImage);
 
   return (
