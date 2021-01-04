c995f31a3af99acef84c1cdd42b35a80af493975
Steven Serrata <sserrata@paloaltonetworks.com>
2021-01-04 11:24:58 -0600

    Move sitemap to theme config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,17 +146,12 @@ module.exports = {
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css")
+        },
+        sitemap: {
+          cacheTime: 600 * 1000, // 600 sec - cache purge period
+          changefreq: "weekly",
+          priority: 0.5
         }
-      }
-    ]
-  ],
-  plugins: [
-    [
-      "@docusaurus/plugin-sitemap",
-      {
-        cacheTime: 600 * 1000, // 600 sec - cache purge period
-        changefreq: "weekly",
-        priority: 0.5
       }
     ]
   ],
