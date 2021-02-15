8fe4115d9b5d08205015c08aec5eda76ce278c49
ikeren <itay@demisto.com>
2021-02-15 16:34:26 +0200

    rm trailing whitespace

--- content-repo/gen_pydocs.py
+++ content-repo/gen_pydocs.py
@@ -168,7 +168,7 @@ def generate_demisto_class_docs(target_dir: str):
     overview = """All Python integrations and scripts have available as part of the runtime the `demisto` class
 object. The object exposes a series of API methods which are used to retrieve and send data to the Cortex XSOAR Server.
 
-:::note 
+:::note
 The `demisto` class is a low level API. For many operations we provide a simpler and more robust API as
 part of the  [Common Server Functions](https://xsoar.pan.dev/docs/integrations/code-conventions#common-server
 -functions).
