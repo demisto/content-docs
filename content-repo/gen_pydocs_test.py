bc49e8aa5d538bdffccbf6a67294749e4742ac9c
ikeren <itay@demisto.com>
2021-02-15 16:50:16 +0200

    add cwd class test helper

--- content-repo/gen_pydocs_test.py
+++ content-repo/gen_pydocs_test.py
@@ -5,6 +5,18 @@ from gen_pydocs import (generate_common_server_python_docs,
                         generate_demisto_class_docs)
 
 
+class CWD:
+    def __init__(self, directory):
+        self.current = os.getcwd()
+        self.directory = directory
+
+    def __enter__(self):
+        os.chdir(self.directory)
+
+    def __exit__(self, *args):
+        os.chdir(self.current)
+
+
 def test_generate_pydoc_demisto_class(tmp_path):
     """
     Given:
@@ -30,29 +42,29 @@ def test_generate_pydoc_demisto_class(tmp_path):
     demisto_class = tmp_path / 'demisto.py'
     with open('./test_data/demistomock.py') as demistomock_file:
         demisto_class.write_text(demistomock_file.read())
-    os.chdir(str(tmp_path))
     sys.path.append(str(tmp_path))
-    generate_demisto_class_docs(str(tmp_path))
-    with open(str(tmp_path / f'{article_id}.md'), 'r') as f:
-        assert f.readline().startswith('---')
-        assert f.readline().startswith(f'id: {article_id}')
-        assert f.readline().startswith(f'title: {article_title}')
-        assert f.readline().startswith(f'description: API reference documentation for {article_title}')
-        assert f.readline().startswith('---')
-        f.readline()
-        assert f.readline().startswith(partial_module_overview)
-        f.readline()
-        f.readline()
-        f.readline()
-        f.readline()
-        f.readline()
-        f.readline()
-        f.readline()
-        f.readline()
-        assert f.readline().startswith('## ')
-        f.readline()
-        assert f.readline().startswith('```python')
-        assert f.readline().startswith(func_prefix)
+    with CWD(str(tmp_path)):
+        generate_demisto_class_docs(str(tmp_path))
+        with open(str(tmp_path / f'{article_id}.md'), 'r') as f:
+            assert f.readline().startswith('---')
+            assert f.readline().startswith(f'id: {article_id}')
+            assert f.readline().startswith(f'title: {article_title}')
+            assert f.readline().startswith(f'description: API reference documentation for {article_title}')
+            assert f.readline().startswith('---')
+            f.readline()
+            assert f.readline().startswith(partial_module_overview)
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            assert f.readline().startswith('## ')
+            f.readline()
+            assert f.readline().startswith('```python')
+            assert f.readline().startswith(func_prefix)
 
 
 def test_generate_pydoc_common_server_python(tmp_path):
@@ -79,16 +91,16 @@ def test_generate_pydoc_common_server_python(tmp_path):
     csp = tmp_path / 'CommonServerPython.py'
     with open('./test_data/CommonServerPython.py') as csp_file:
         csp.write_text(csp_file.read())
-    os.chdir(str(tmp_path))
     sys.path.append(str(tmp_path))
-    generate_common_server_python_docs(str(tmp_path))
-    with open(str(tmp_path / f'{article_id}.md'), 'r') as f:
-        assert f.readline().startswith('---')
-        assert f.readline().startswith(f'id: {article_id}')
-        assert f.readline().startswith(f'title: {article_title}')
-        assert f.readline().startswith(f'description: API reference documentation for {article_title}')
-        assert f.readline().startswith('---')
-        f.readline()
-        assert f.readline().startswith(module_overview)
-        f.readline()
-        assert f.readline().startswith('## ')
+    with CWD(str(tmp_path)):
+        generate_common_server_python_docs(str(tmp_path))
+        with open(str(tmp_path / f'{article_id}.md'), 'r') as f:
+            assert f.readline().startswith('---')
+            assert f.readline().startswith(f'id: {article_id}')
+            assert f.readline().startswith(f'title: {article_title}')
+            assert f.readline().startswith(f'description: API reference documentation for {article_title}')
+            assert f.readline().startswith('---')
+            f.readline()
+            assert f.readline().startswith(module_overview)
+            f.readline()
+            assert f.readline().startswith('## ')
