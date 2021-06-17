84e0bf3795f59f3602d689775f2fba254da72675
glicht <glicht@users.noreply.github.com>
2021-06-17 19:56:42 +0300

    try to fix mp sidebar error

--- src/theme/MarketplaceSidebar/index.js
+++ src/theme/MarketplaceSidebar/index.js
@@ -317,6 +317,19 @@ function DocSidebarItem(props) {
   }
 }
 
+function useShowAnnouncementBar() {
+  const {isAnnouncementBarClosed} = useUserPreferencesContext();
+  const [showAnnouncementBar, setShowAnnouncementBar] = useState(
+    !isAnnouncementBarClosed,
+  );
+  useScrollPosition(({scrollY}) => {
+    if (!isAnnouncementBarClosed) {
+      setShowAnnouncementBar(scrollY === 0);
+    }
+  });
+  return showAnnouncementBar;
+}
+
 function MarketplaceSidebar({
   path,
   sidebar,
@@ -327,6 +340,7 @@ function MarketplaceSidebar({
   totalPacks,
   totalFilteredPacks,
 }) {
+  const showAnnouncementBar = useShowAnnouncementBar();
   const [showResponsiveSidebar, setShowResponsiveSidebar] = useState(false);
   const {
     navbar: { hideOnScroll },
@@ -357,8 +371,7 @@ function MarketplaceSidebar({
           styles.menu,
           {
             "menu--show": showResponsiveSidebar,
-            [styles.menuWithAnnouncementBar]:
-              !isAnnouncementBarClosed && scrollY === 0,
+            [styles.menuWithAnnouncementBar]: showAnnouncementBar              
           }
         )}
       >
