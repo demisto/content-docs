c74d5ca361be08c05bd0740c77c227bfd530a8d8
dkoval <dkoval@paloaltonetworks.com>
2021-08-24 12:11:10 +0300

    added croped photos

--- src/pages/marketplace/slideshow.js
+++ src/pages/marketplace/slideshow.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import Link from "@docusaurus/Link";
+import image1 from './new_images/Marketplace_Vertical_1.png';
+import image2 from './new_images/Marketplace_Vertical_2.png';
+import image3 from './new_images/Marketplace_Vertical_3.png';
 
 
 const slideImages = [
   {
-    url: 'https://raw.githubusercontent.com/demisto/content-docs/148c5a9e7153b1688acb2f331b414e30a3f75f8c/src/pages/marketplace/images/Marketplace_Vertical_1.png',
+    url: image1, //'https://raw.githubusercontent.com/demisto/content-docs/148c5a9e7153b1688acb2f331b414e30a3f75f8c/src/pages/marketplace/images/Marketplace_Vertical_1.png',
     caption: 'Slide 1',
     link: 'https://start.paloaltonetworks.com/join-our-slack-community'
   },
   {
-    url: 'https://raw.githubusercontent.com/demisto/content-docs/148c5a9e7153b1688acb2f331b414e30a3f75f8c/src/pages/marketplace/images/Marketplace_Vertical_2.png',
+    url: image2, //'https://raw.githubusercontent.com/demisto/content-docs/148c5a9e7153b1688acb2f331b414e30a3f75f8c/src/pages/marketplace/images/Marketplace_Vertical_2.png',
     caption: 'Slide 2',
     link: 'https://start.paloaltonetworks.com/cortex-xsoar-whats-soaring-newsletter.html'
   },
   {
-    url: 'https://raw.githubusercontent.com/demisto/content-docs/148c5a9e7153b1688acb2f331b414e30a3f75f8c/src/pages/marketplace/images/Marketplace_Vertical_3.png',
+    url: image3, //'https://raw.githubusercontent.com/demisto/content-docs/148c5a9e7153b1688acb2f331b414e30a3f75f8c/src/pages/marketplace/images/Marketplace_Vertical_3.png',
     caption: 'Slide 3',
     link: 'https://www.paloaltonetworks.com/cortex/xsoar'
   }
