d880f6bff7b8046f48a44c7c81f14f86751062bc
marcellarichmond <69267006+marcellarichmond@users.noreply.github.com>
2021-10-04 12:56:30 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -234,6 +234,7 @@ const sidebars = {
         "partners/become-a-tech-partner",
         "partners/paid-packs",
         "partners/adopt",
+        "partner/benefits",
         "partners/certification",
         "partners/office-hours",
         "partners/development-partners"
