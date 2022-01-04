64a40e97f05da11d1efaad13e4cfd146f404443b
Dan <dansterenson@gmail.com>
2022-01-04 13:07:22 +0200

    add slack notifier

--- build_utils/slack_notifier.py
+++ build_utils/slack_notifier.py
@@ -1,35 +1,29 @@
 import os
 import sys
+import argparse
 
 from slackclient import SlackClient  # type: ignore
 
 
-def create_slack_notifier(slack_token, job_status):
-    print("Extracting content-docs build status")
+def create_slack_notifier(slack_token):
     print("Sending Slack messages to #dmst-dummy")
-    if job_status == 'failure' or job_status == 'success':
-        slack_client = SlackClient(slack_token)
-        slack_client.api_call(
-            "chat.postMessage",
-            channel="dmst-dummy",
-            username="Content CircleCI",
-            as_user="False",
-        )
+    slack_client = SlackClient(slack_token)
+    slack_client.api_call(
+        "chat.postMessage",
+        channel="dmst-dummy",
+        username="Content-Docs CircleCI",
+        as_user="False",
+    )
 
 
 def main():
-    slack_token = os.environ['SLACK_TOKEN']
-    job_status = sys.argv[1]
+    slack_token = sys.argv[1]
 
     if not slack_token:
         print('Error: Slack token is not configured')
         exit(1)
 
-    elif not job_status:
-        print('Error: Job status is unknown')
-        exit(1)
-
-    create_slack_notifier(slack_token=slack_token, job_status=job_status)
+    create_slack_notifier(slack_token=slack_token)
 
 
 if __name__ == '__main__':
