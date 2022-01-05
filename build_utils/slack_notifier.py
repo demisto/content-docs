4ec94a0c8fd7829f9cce7ddddd60e7c45f0eb7ea
Dan <dansterenson@gmail.com>
2022-01-05 13:14:25 +0200

    change to slack sdk

--- build_utils/slack_notifier.py
+++ build_utils/slack_notifier.py
@@ -2,28 +2,32 @@ import os
 import sys
 import argparse
 
-from slackclient import SlackClient  # type: ignore
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 
-def create_slack_notifier(slack_token):
+def create_slack_notifier(slack_token, build_url):
     print("Sending Slack messages to #dmst-dummy")
-    slack_client = SlackClient(slack_token)
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="dmst-dummy",
-        username="Content-Docs CircleCI",
-        as_user="False",
-    )
+    try:
+        slack_client = WebClient(token=slack_token)
+        slack_client.chat_postMessage(
+            channel="dan-test-channel",
+            text=build_url,
+            as_user=False,
+            username="Content-Docs CircleCI"
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
 
 
 def main():
     slack_token = sys.argv[1]
-
+    build_url = sys.argv[2]
     if not slack_token:
         print('Error: Slack token is not configured')
         exit(1)
 
-    create_slack_notifier(slack_token=slack_token)
+    create_slack_notifier(slack_token=slack_token, build_url=build_url)
 
 
 if __name__ == '__main__':
