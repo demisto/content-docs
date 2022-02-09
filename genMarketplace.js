e18b95bfdf42d27fd4c55083996a93f0bd1b1555
Dean Arbel <darbel@paloaltonetworks.com>
2022-02-09 14:19:38 +0200

    Marketplace Download Packs (#874)
    * add download button

--- genMarketplace.js
+++ genMarketplace.js
@@ -85,6 +85,95 @@ function createReadmeLink(listItem, itemType) {
   return Promise.resolve();
 }
 
+function travelDependenciesJson(firstLvlDepsJson, depsJson, startKey) {
+  /* Travels over the dependencies json to create a depdendencies map for each entry starting with startKey like so:
+  {
+    startKey: {
+      mandatory: {
+        packName: {
+          version: version
+        }
+      },
+      optional: {
+        packName: {
+          version: version
+        }
+      },
+      version: "...",
+      "mandatoryCount": ...,
+      "optionalCount": ...
+    }
+  }
+  */
+  // don't travel dependencies JSON if already travelled from the startKey
+  if (!(startKey in depsJson)) {
+    travelDependencies(depsJson, startKey, firstLvlDepsJson);
+    depsJson[startKey]['mandatoryCount'] = Object.keys(depsJson[startKey]['mandatory']).length;
+    depsJson[startKey]['optionalCount'] = Object.keys(depsJson[startKey]['optional']).length;
+  }
+}
+
+function travelDependencies(depsJson, startKey, firstLvlDepsJson) {
+  // Travel over the dependencies json of a given Pack dependency type, while collecting all sub-dependecy mandatory packs
+  
+  if (!(startKey in depsJson)) {
+    if (!(startKey in firstLvlDepsJson)) {
+      depsJson[startKey] = {'mandatory': {}, 'optional': {}}
+    } else {
+      depsJson[startKey] = {...firstLvlDepsJson[startKey]};
+    }
+  }
+
+  dependencyPacks = depsJson[startKey]['mandatory'];
+  for (var depKey in dependencyPacks) {
+    if (depsJson[depKey] === undefined) {
+      travelDependenciesJson(firstLvlDepsJson, depsJson, depKey);
+    }
+    
+    // fill mandatory sub-dependecies
+    for (var subDepKey in depsJson[depKey]['mandatory']) {
+      // skip if subDepKey is startKey or if subDepKey is already in node's collected mandatory
+      if (subDepKey !== startKey && (dependencyPacks[subDepKey] === undefined) ) {
+        if (depsJson[subDepKey] === undefined) {
+          // subDepKey wasn't yet traveled
+          travelDependenciesJson(firstLvlDepsJson, depsJson, subDepKey);
+        }
+        // collect subDepKey chained mandatory dependecies (subDepKey incl.)
+        for (var chainKey in getMandatoryChainDependencies(subDepKey, depsJson)) {
+          depsJson[startKey]['mandatory'][chainKey] = {
+            version: depsJson[chainKey].version,
+            support: depsJson[chainKey].support === "xsoar" ? "Cortex XSOAR" : capitalizeFirstLetter(depsJson[chainKey].support)
+          }
+        }
+      }
+    }
+  }
+}
+
+function getMandatoryChainDependencies(packName, depsJson, exclusionSet) {
+  // collects all mandatory chained dependencies, while skipping over keys it collected along the way
+
+  res = {};
+  if (!(packName in depsJson)) {  // sanity
+    return res;
+  }
+  if (exclusionSet === undefined) {
+    exclusionSet = new Set(packName);
+  }
+
+  for (var depKey in depsJson[packName]['mandatory']) {
+    // prevent iterating over a key that was already iterated
+    if (!exclusionSet.has(depKey)) {
+      exclusionSet.add(depKey);
+      res = {
+        ...res, 
+        ...getMandatoryChainDependencies(depKey, depsJson, exclusionSet)};
+        res[packName] = {version: depsJson[packName].version};
+    }
+  }
+  return res
+}
+
 function reverseReleases(obj) {
   let new_obj = {};
   let rev_obj = Object.keys(obj).reverse();
@@ -96,6 +185,9 @@ function reverseReleases(obj) {
 
 function genPackDetails() {
   let marketplace = [];
+  let idToPackMetadata = {};
+  const firstLeveldepsMap = {};  // map of dependencies per pack ID (first level)
+  const fullDepsJson = {};  // map of dependencies per pack ID (all levels)
   const detailsPages = globby.sync(["./src/pages/marketplace"], {
     absolute: false,
     objectMode: true,
@@ -138,7 +230,8 @@ function genPackDetails() {
           year: "numeric",
           month: "long",
           day: "numeric",
-        });
+        }),
+        release.version = release.displayName.split(' ')[0];
       }
     }
     metadata.changeLog = changeLog;
@@ -152,8 +245,38 @@ function genPackDetails() {
     } else {
       console.log("no README.md for", metadata.name);
     }
+    idToPackMetadata[metadata.id] = {
+      version: metadata.currentVersion,
+      support: metadata.support === "xsoar" ? "Cortex XSOAR" : capitalizeFirstLetter(metadata.support)
+    };
     marketplace.push(metadata);
   });
+
+  marketplace.map((metadata) => {
+    if (metadata.dependencies) {
+      let dependenciesJson = {
+        mandatory: {},
+        optional: {},
+        version: metadata.currentVersion,
+        support: metadata.support
+      };
+      for (var depId in metadata.dependencies) {
+        let dependency = metadata.dependencies[depId]
+        if (dependency.mandatory) {
+          dependenciesJson["mandatory"][depId] = {
+            version: idToPackMetadata[depId].version,
+            support: idToPackMetadata[depId].support
+          };
+        } else {
+          dependenciesJson["optional"][depId] = {
+            version: idToPackMetadata[depId].version,
+            support: idToPackMetadata[depId].support
+          };
+        }
+      }
+      firstLeveldepsMap[metadata.id] = dependenciesJson;
+    }
+  })
 
   if (process.env.MAX_PACKS) {
     console.log(`limiting packs to ${process.env.MAX_PACKS}`);
@@ -200,8 +323,18 @@ function genPackDetails() {
       }
     }
 
-    await parseContentItems()
+    await parseContentItems();
 
+    try {
+      if (pack.dependencies) {
+        if (!(pack.id in fullDepsJson)) {
+          travelDependenciesJson(firstLeveldepsMap, fullDepsJson, pack.id)
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    
     generatePackDetails.runActions({
       id: pack.id ? pack.id.replace(/-|\s/g, "").replace(".", "") : pack.id,
       name: pack.name,
@@ -232,6 +365,7 @@ function genPackDetails() {
       integrations: pack.integrations,
       contentItems: pack.contentItems,
       changeLog: reverseReleases(pack.changeLog),
+      dependencies: fullDepsJson[pack.id]
     });
   });
 };
