ce5341d0987058bfbe4c420720865419ac54ce96
ShirleyDenkberg <62508050+ShirleyDenkberg@users.noreply.github.com>
2022-03-20 09:53:07 +0200

    Add generic-endpoint-command (#1024)
    * Add generic-endpoint-command
    
    * Update sidebars.js
    
    Co-authored-by: ostolero <86190583+ostolero@users.noreply.github.com>
    
    * Update sidebars.js
    
    done.
    
    Co-authored-by: ostolero <86190583+ostolero@users.noreply.github.com>
    Co-authored-by: ostolero <ostolero@paloaltonetworks.com>

--- sidebars.js
+++ sidebars.js
@@ -113,7 +113,8 @@ const sidebars = {
               label: "Generic Commands",
               items: [
                 "integrations/generic-commands-about",
-                "integrations/generic-commands-reputation"
+                "integrations/generic-commands-reputation",
+                "integrations/generic-endpoint-command",
               ]
             },
             "integrations/dbot",
