c09b8df8d717425c59c5347cc1d23a48786b42ad
meichlerpanw <meichler@paloaltonetworks.com>
2022-03-24 12:37:21 +0200

    branch

--- content-repo/create-docs.sh
+++ content-repo/create-docs.sh
@@ -29,7 +29,7 @@ else
     echo "==== current branch: ${CURRENT_BRANCH} ===="
 
     CONTENT_GIT_URL="https://github.com/demisto/content.git"
-    CONTENT_BRANCH="improve_demisto_results_description"
+    CONTENT_BRANCH="Improve_result_description"
     REQUIRE_BRANCH=false
     if [ -n "${INCOMING_HOOK_BODY}" ]; then
         echo "INCOMING_HOOK_BODY=${INCOMING_HOOK_BODY}"
