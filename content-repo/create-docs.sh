2183739e231f6633597d7b97fafcbfa647e3c34b
meichlerpanw <meichler@paloaltonetworks.com>
2022-03-30 14:38:37 +0300

    1

--- content-repo/create-docs.sh
+++ content-repo/create-docs.sh
@@ -53,15 +53,8 @@ else
     fi
 
     if [ ! -d ${CONTENT_GIT_DIR} ]; then
-
-        if [[ "$CONTENT_BRANCH" == "master" ]]; then
-          echo "Cloning content (depth 1) to dir: ${CONTENT_GIT_DIR} ..."
-          git clone --depth 1 ${CONTENT_GIT_URL} ${CONTENT_GIT_DIR}
-        else
-          echo "Cloning content to dir: ${CONTENT_GIT_DIR} ..."
-          git clone ${CONTENT_GIT_URL} ${CONTENT_GIT_DIR}
-        fi
-
+        echo "Cloning content to dir: ${CONTENT_GIT_DIR} ..."
+        git clone --depth 1 ${CONTENT_GIT_URL} ${CONTENT_GIT_DIR}
     else
         echo "Content dir: ${CONTENT_GIT_DIR} exists. Skipped clone."
         if [ -z "${CONTENT_REPO_SKIP_PULL}"]; then        
