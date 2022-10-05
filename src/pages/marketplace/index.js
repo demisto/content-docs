f238818fd4fa8e06b3de0b92aa8e24e10adf4f32
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-10-05 11:43:16 -0400

    Fix marketplace redirects (#1201)
    * Fix marketplace redirects
    
    * Add marketplace client-side redirect

---
+++ src/pages/marketplace/index.js
@@ -0,0 +1,25 @@
+import { useLocation } from "@docusaurus/router";
+import React, { useEffect } from "react";
+
+function Marketplace() {
+  const location = useLocation();
+  const newLocation = location.search
+    ? `https://cortex.marketplace.pan.dev/marketplace/${location.search}`
+    : "https://cortex.marketplace.pan.dev/marketplace";
+
+  useEffect(() => {
+    window.location.href = newLocation;
+  }, []);
+
+  return (
+    <span>
+      Redirecting to Cortex XSOAR Marketplace... click{" "}
+      <a target="_self" href={newLocation}>
+        here
+      </a>{" "}
+      if the redirect fails or is taking too long.
+    </span>
+  );
+}
+
+export default Marketplace;
