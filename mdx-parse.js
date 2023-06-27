f94e58dc0ef56105786d8fee7ec8af5a1951ec57
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2023-06-27 10:51:44 +0300

    Fix @mdx-js/mdx require issue (no longer supported on newer versions)

--- mdx-parse.js
+++ mdx-parse.js
@@ -1,14 +1,14 @@
 const cli = require('commander');
 const {readFile} = require('fs-extra');
-const mdx = require('@mdx-js/mdx');
 
 cli.version('0.1.0')
 cli.requiredOption("-f --file <mdx file to parse>")
 cli.parse(process.argv)
 
 async function parseMDX(file) {
+    const mdx = await import('@mdx-js/mdx');
     const contents = await readFile(file, 'utf8');
-    parsed = await mdx(contents)    
+    await mdx.default(contents)
     console.log("parsed successfully!")
 }
 
