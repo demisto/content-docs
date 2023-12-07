835e86459c09967098d6e57f043368d5657ad522
MosheEichler <meichler@paloaltonetworks.com>
2023-12-07 15:32:38 +0200

    fix mdx parser

--- mdx-parse.mjs
+++ mdx-parse.mjs
@@ -1,14 +1,14 @@
-const cli = require('commander');
-const {readFile} = require('fs-extra');
-const mdx = require('@mdx-js/mdx');
+import cli from 'commander';
+import fs from 'fs-extra';
+import {compile} from '@mdx-js/mdx';
 
 cli.version('0.1.0')
 cli.requiredOption("-f --file <mdx file to parse>")
 cli.parse(process.argv)
 
 async function parseMDX(file) {
-    const contents = await readFile(file, 'utf8');
-    parsed = await mdx(contents)    
+    const contents = await fs.readFile(file, 'utf8');
+    const parsed = await compile(contents)
     console.log("parsed successfully!")
 }
 
