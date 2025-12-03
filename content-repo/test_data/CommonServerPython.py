a46c1abb6929e37d01d903f41c32f88f165b6517
hyaffe <hyaffe@paloaltonetworks.com>
2025-12-03 19:10:29 +0200

    runPlaybook

--- content-repo/test_data/CommonServerPython.py
+++ content-repo/test_data/CommonServerPython.py
@@ -64,7 +64,7 @@ except Exception:
         sys.exc_clear()
 
 CONTENT_RELEASE_VERSION = '0.0.0'
-CONTENT_BRANCH_NAME = 'runPlaybook3'
+CONTENT_BRANCH_NAME = 'runPlaybook'
 IS_PY3 = sys.version_info[0] == 3
 
 # pylint: disable=undefined-variable
