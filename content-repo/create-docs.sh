062557c3cb7d4dca50958dba41f89cb049649d8d
ShacharKidor <skidorball@paloaltonetworks.com>
2026-09-07 15:35:54 +0300

    removed comment

--- content-repo/create-docs.sh
+++ content-repo/create-docs.sh
@@ -3,7 +3,7 @@
 # exit on errors
 set -e
 
-# Script will check out the Demisto content repo and then generate documentation based upon the checkout - comment to trigger the build
+# Script will check out the Demisto content repo and then generate documentation based upon the checkout
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
 CURRENT_DIR=$(pwd)
